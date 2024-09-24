7ccd8ab9201427542ff2008ccd4e319c952db511
MmbiMasana <mmbimasana@gmail.com>
2024-09-24 23:07:56 +0200

    committed

--- script.js
+++ script.js
@@ -21,17 +21,40 @@ function determineWinner(playerChoice, computerChoice) {
     }
 }
 
-// Function to play a round of Rock, Paper, Scissors
+// Function to play 5 rounds of Rock, Paper, Scissors
 function playGame() {
-    const playerChoice = prompt('Enter rock, paper, or scissors:').toLowerCase();
-    const computerChoice = getComputerChoice();
-
-    console.log(`You chose: ${playerChoice}`);
-    console.log(`Computer chose: ${computerChoice}`);
-    
-    const result = determineWinner(playerChoice, computerChoice);
-    console.log(result);
-}
+    let playerScore = 0;
+    let computerScore = 0;
+  
+    for (let round = 1; round <= 5; round++) {
+      const playerChoice = prompt(`Round ${round} - Enter Rock, Paper, or Scissors:`).toLowerCase();
+      const computerChoice = getComputerChoice();
+      const result = determineWinner(playerChoice, computerChoice);
+  
+      console.log(`Round ${round}:`);
+      console.log(`You chose: ${playerChoice}`);
+      console.log(`Computer chose: ${computerChoice}`);
+      console.log(result);
+  
+      if (result === "You win!") {
+        playerScore++;
+      } else if (result === "You lose!") {
+        computerScore++;
+      }
+  
+      console.log(`Score: Player ${playerScore} - Computer ${computerScore}`);
+      console.log("-----------------------------");
+    }
+  
+    // Determine the final winner
+    if (playerScore > computerScore) {
+      console.log("Congratulations! You won the game!");
+    } else if (playerScore < computerScore) {
+      console.log("Oh no! You lost the game.");
+    } else {
+      console.log("It's a tie overall!");
+    }
+  }
 
 // Start the game
 playGame();
